b3dad94e6fb544f2733c714b999f0f7e05865ca4
zhangjppig <2590965263@qq.com>
2022-05-03 16:13:56 +0800

    feat：增加v-header组件

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,17 @@
 <template>
   <div id="app">
-    <div>
-      <label for="name">name: </label>
-      <cube-input v-model="name"></cube-input>
-    </div>
-    <cube-button class="bottom-btn" @click="sayHi">click</cube-button>
+    <v-header></v-header>
   </div>
 </template>
 
 <script>
+import VHeader from "components/v-header/v-header";
 export default {
-  name: 'App',
-  data() {
-    return {
-      name: 'Tom'
-    }
+  name: "App",
+  components: {
+    VHeader,
   },
-  methods: {
-    sayHi() {
-      this.$createDialog({
-        type: 'alert',
-        title: 'Hello',
-        content: this.name
-      }).show()
-    }
-  }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.bottom-btn {
-  position: fixed;
-  bottom: 0;
-}
-</style>
+<style lang="stylus"></style>
